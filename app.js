02d6ff188446c094ca765b362e79164b709bd61c
Vinny Parra <vinny.parra@gmail.com>
2026-02-21 22:24:14 -0600

    Add simple Express server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello from WSL 🚀");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Simple route
+app.get("/", (req, res) => {
+  res.send("Hello from Vinny's Node server 🚀");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
